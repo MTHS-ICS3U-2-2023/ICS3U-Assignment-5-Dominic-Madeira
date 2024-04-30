dd390e71ce671cfdfb158ec9d7133abd4cd102dc
Dominic Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2024-04-30 11:07:37 -0400

    done py again

--- main.py
+++ main.py
@@ -6,29 +6,6 @@ This module calculates the GCF of two numbers
 """
 
 
-def greatest_common_factor(number1: int, number2: int) -> int:
-    """This function calculates the GCF of two numbers, returns GCF."""
-
-    # process
-    # find the smallest number
-    if number1 > number2:
-        counter = number2
-    else:
-        counter = number1
-
-    # find the GCF
-    while counter > 0:
-        divider1 = number1 / counter
-        divider2 = number2 / counter
-        if divider1 % 1 == 0 and divider2 % 1 == 0:
-            gcf = counter
-            break
-        else:
-            counter -= 1
-
-    return gcf
-
-
 def main() -> None:
     """The main() function gets input, calls other functions, shows output,
     returns None."""
@@ -44,11 +21,25 @@ def main() -> None:
         print("\nYou did not enter a valid number.")
     else:
 
-        # call the function
-        gcf = greatest_common_factor(number1, number2)
+        # process
+        # find the smallest number
+        if number1 > number2:
+            counter = number2
+        else:
+            counter = number1
+
+        # find the GCF
+        while counter > 0:
+            divider1 = number1 / counter
+            divider2 = number2 / counter
+            if divider1 % 1 == 0 and divider2 % 1 == 0:
+                gcf = counter
+                break
+            else:
+                counter -= 1
 
         # output
-        print(f"The greatest common factor is {gcf}.")
+        print(f"\nThe greatest common factor is {gcf}.")
 
     print("\nDone.")
 
